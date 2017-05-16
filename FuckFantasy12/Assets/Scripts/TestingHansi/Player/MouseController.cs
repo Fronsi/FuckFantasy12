68be6ac7c6309765567ff1e4e98e9c75e126a87a
Zockerjimmy <alexander_schmidt2@hotmail.de>
2017-05-17 00:24:22 +0200

    MouseMovement

--- FuckFantasy12/Assets/Scripts/TestingHansi/Player/MouseController.cs
+++ FuckFantasy12/Assets/Scripts/TestingHansi/Player/MouseController.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class MouseController : MonoBehaviour {
 
+    public enum eRotationAxis { MouseY, MouseX};
+    public eRotationAxis cameraDirection = eRotationAxis.MouseY;
+    public float fRotationSpeedX = 10.0f;
+    public float fRotationSpeedY = 10.0f;
+
+    private float xMin = -60.0f;
+    private float xMax = 60.0f;
+    private float fRotationX = 0.0f;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked; ;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        if (cameraDirection == eRotationAxis.MouseY)
+        {
+            transform.Rotate(0.0f, Input.GetAxis("Mouse X") * fRotationSpeedY, 0.0f);
+        }
+        if (cameraDirection == eRotationAxis.MouseX)
+        {
+            fRotationX += Input.GetAxis("Mouse Y") * fRotationSpeedX;
+            fRotationX = Mathf.Clamp(fRotationX, xMin, xMax);
+            transform.localEulerAngles = new Vector3(-fRotationX, transform.localEulerAngles.y, 0.0f);
+        }
 	}
 }
